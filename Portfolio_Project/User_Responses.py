c0ccd9bf992c6787e4afa387980b3e4b740d2812
Shannon Webster <shanoonwebbie@gmail.com>
2023-08-12 17:20:54 -0700

    working on my builder method.

--- Portfolio_Project/User_Responses.py
+++ Portfolio_Project/User_Responses.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
+from Portfolio_Project.User_Input import get_user_input
 
 
 @dataclass()
@@ -19,11 +20,17 @@ class UserDrivenResponsesBuilder:
             UserWantsRecentlyPublished= self.UserWantsRecentlyPublished,
             DoesUserWantStaffRecommendations= self.DoesUserWantStaffRecommendations,
             UserProvidedThisTitle= self.UserProvidedThisTitle
-
-
         )
 
         return new_responses
+
+    def add_genre_from_user(self) -> "UserDrivenResponsesBuilder":
+        question = "Which genre of book would you like to read?"
+        answer = get_user_input(question)
+        self.GenreFromUser = answer
+
+        return self
+
     def __init__(self):
         self.GenreFromUser = None
         self.DoesUserWantFiction = False
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,4 +1,5 @@
 from Portfolio_Project.User_Responses import UserDrivenResponsesBuilder, UserDrivenResponses
+from unittest.mock import patch
 
 def test_builder_builds_userdrivenresponses():
     Builder = UserDrivenResponsesBuilder()
@@ -6,3 +7,12 @@ def test_builder_builds_userdrivenresponses():
     actual = Builder.build()
     assert isinstance(actual,UserDrivenResponses)
 
+def test_add_genre_from_user_returns_builder():
+    Builder = UserDrivenResponsesBuilder()
+    example_genre = "Fantasy"
+    with patch("Portfolio_Project.User_Responses.get_user_input", return_value=example_genre) as input_mock:
+        actual = Builder.add_genre_from_user()
+    input_mock.assert_called()
+    assert isinstance(actual, UserDrivenResponsesBuilder)
+    assert Builder.GenreFromUser is not None
+    assert Builder.GenreFromUser == example_genre
